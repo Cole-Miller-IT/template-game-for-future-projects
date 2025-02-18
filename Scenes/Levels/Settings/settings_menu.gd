9aa917d9af18ad36b1184b5b05c1faf8542452de
Cole-Miller-IT <68202945+Cole-Miller-IT@users.noreply.github.com>
2025-02-18 13:50:36 -0500

    working on rebidable keys

--- Scenes/Levels/Settings/settings_menu.gd
+++ Scenes/Levels/Settings/settings_menu.gd
@@ -1,22 +1,22 @@
 extends CanvasLayer
 
 
+var rebindMode = false
+var rebindKeyToChange = ""
+
+@onready var hotkeys = load("res://Scenes/Resources/Settings/keybinds.tres")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#Read hotkey resource data and assign the buttons the correct keys to display on the rebind button
-	var hotkeys = load("res://Scenes/Resources/Settings/keybinds.tres")
-	
 	if hotkeys:
-		print("loaded hotkeys")
-		print(hotkeys.defaultMoveLeft)
-		
 		$MarginContainer/VBoxContainer/TabContainer/Keybinds/MarginContainer/VBoxContainer/MoveLeft/RebindKey1.text = hotkeys.defaultMoveLeft
 		$MarginContainer/VBoxContainer/TabContainer/Keybinds/MarginContainer/VBoxContainer/MoveRight/RebindKey1.text = hotkeys.defaultMoveRight
 		$MarginContainer/VBoxContainer/TabContainer/Keybinds/MarginContainer/VBoxContainer/MoveUp/RebindKey1.text = hotkeys.defaultMoveUp
 		$MarginContainer/VBoxContainer/TabContainer/Keybinds/MarginContainer/VBoxContainer/MoveDown/RebindKey1.text = hotkeys.defaultMoveDown
 		#$MarginContainer/VBoxContainer/TabContainer/Keybinds/MarginContainer/VBoxContainer/LightAttack/RebindKey1.text = hotkeys.defaultLightAttack
 	
-
+		print("loaded hotkeys succesfully")
 
 
 func _on_check_button_toggled(toggled_on):
@@ -37,12 +37,45 @@ func _on_cancel_pressed():
 
 
 ###############REBINDING HOTKEYS#######################
-func _on_rebind_key_pressed():
-	print("Awaiting key press")
+func _on_rebind_key_pressed(value):
+	#print("Awaiting key press for hotkey " + str(value))
 	
-	#Wait for the next input from the user
+	#toggle rebind mode
+	#rebindMode = true
+	
+	#handle next input event through _unhandled_input() function
+	pass
+	
+func rebind(eventRecieved):
+	#print(eventRecieved)
+	#rebind the key to the given key by the user
+	if rebindKeyToChange == "moveLeft":
+		#this
+		#work to change the key code to the key value (ie 71 to g)
+		hotkeys.defaultMoveLeft = str(eventRecieved.keycode)
+		print(str(eventRecieved.keycode))
+		
+	else:
+		print("hotkey rebind not implemented yet")
+	
+	#
+	#make this a variable string for all hotkeys
+	#
+	$MarginContainer/VBoxContainer/TabContainer/Keybinds/MarginContainer/VBoxContainer/MoveLeft/RebindKey1.text = str(eventRecieved)
+	
+	rebindMode = false
 	
 	
-	#rebind the key
+func _unhandled_input(event):
+	#print(str(event))
+	if rebindMode == true:
+		#print("changing")
+		rebind(event)
 	
 	
+
+
+func _on_rebind_key_1_pressed(extra_arg_0):
+	print(extra_arg_0)
+	rebindMode = true
+	rebindKeyToChange = extra_arg_0
